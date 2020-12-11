a2286d0cd0fd70bdff75a30dc68f895d52773e46
Amh <abaams78@gmail.com>
2020-12-11 17:28:24 -0600

    spring event project setup

---
+++ src/main/java/com/example/SpringEventApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintEventApplication.class, args);
+	}
+
+}
